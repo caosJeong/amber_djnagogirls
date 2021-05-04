19531f31cf737f2191330404c40ef91ecda52189
amber <amber.jng@kidsnote.com>
2021-05-04 17:40:21 +0900

    꾸다네 가족 데이빗 PR 반영 - 가족 구성원 추가/수정/삭제 시 form_class 적용 - 추가/수정시 validatior 적용 - 구성원 수정시 수정 불가 항목 적용

--- members/urls.py
+++ members/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.urls import path
 from .views import (
-    # 홈화면
+    # 구성원 목록
     HomeView,
-    # 구성원 상세 화면
+    # 구성원 상세, 추가, 수정, 삭제 화면
     MemberDetailView,
-    # 구성원 추가 화면
-    MemberAddView
+    MemberAddView,
+    MemberUpdateView,
+    MemberDeleteView,
 
 )
 app_name = 'member'
@@ -14,4 +15,6 @@ urlpatterns = [
     path('',  HomeView.as_view(), name='member_list'),
     path('<int:pk>/', MemberDetailView.as_view(), name='member_detail'),
     path('add/', MemberAddView.as_view(), name='member_add'),
+    path('update/<int:pk>/', MemberUpdateView.as_view(), name='member_update'),
+    path('delete/<int:pk>/', MemberDeleteView.as_view(), name='member_delete'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.urls import reverse, reverse_lazy
-from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from django.views import generic
-from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
+from members.forms import MemberFrom, MemberUpdateFrom
 from members.models import Member
 
 
@@ -22,15 +22,35 @@ class MemberDetailView(generic.DetailView):
     model = Member
 
 
-class MemberAddView(generic.CreateView):
+class MemberAddView(CreateView):
     """
     구성원 추가 화면
     """
+    form_class = MemberFrom
+    template_name = 'members/member_form.html'
+
+    def get_success_url(self):
+        return reverse('member:member_list')
+
+    def form_invalid(self, form):
+        """If the form is invalid, render the invalid form."""
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class MemberUpdateView(UpdateView):
+    """
+    구성원 수정 화면
+    """
     model = Member
-    fields = ['member_legs', 'member_name', 'member_gender', 'member_birthday', 'member_description',
-              'member_regular', 'member_feature', 'member_feature', 'member_neuter',
-              'member_color', 'member_language', 'member_sleep_time', 'member_talent', 'member_img_name', ]
-    template_name = 'members/members_form.html'
+    form_class = MemberUpdateFrom
+    template_name = 'members/member_update.html'
+
+
+class MemberDeleteView(DeleteView):
+    """
+    구성원 삭제 화면
+    """
+    model = Member
 
     def get_success_url(self):
         return reverse('member:member_list')
